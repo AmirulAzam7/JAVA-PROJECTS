af997d4f96f03d4d19cd3e29b3a221909841df6a
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2023-03-27 12:27:04 +0600

    Add files via upload

---
+++ Quiz-Application-Using-Java-master/src/quiz/application/Rules.java
@@ -0,0 +1,72 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Read the questions carefully." + "<br><br>" +
+                "2. There will be 10 questions and you have to answer all of them." + "<br><br>" +
+                "3. You will get 15 seconds for every question." + "<br><br>" +
+                "4. Try to answer as much as possible." + "<br><br>" +
+                "5. Stay calm,and try to remember everything." + "<br><br>" +
+                "6. You will rewarded 10000 taka if you win this quiz." + "<br><br>" +
+                "7. Don't get nervous,you can do it." + "<br><br>" +
+                "8. BEST OF LUCK." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
